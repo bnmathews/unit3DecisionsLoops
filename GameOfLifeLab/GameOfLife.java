2c720e58961f7966f9b85946cc4dfe1602ddabd2
Ben Mathews <bnmathews@student.naperville203.org>
2014-11-08 06:12:41 -0600

    GameofLife Progressssssss

--- GameOfLifeLab/GameOfLife.java
+++ GameOfLifeLab/GameOfLife.java
@@ -4,6 +4,8 @@ import info.gridworld.actor.Rock;
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.BoundedGrid;
 import info.gridworld.grid.Location;
+import java.util.ArrayList;
+import java.awt.Color;
 
 /**
  * Game of Life starter code. Demonstrates how to create and populate the game using the GridWorld framework.
@@ -18,8 +20,8 @@ public class GameOfLife
     private ActorWorld world;
     
     // the game board will have 5 rows and 5 columns
-    private final int ROWS = 20;
-    private final int COLS = 29;
+    private final int ROWS = 21;
+    private final int COLS = 30;
 
     /**
      * Default constructor for objects of class GameOfLife
@@ -31,6 +33,7 @@ public class GameOfLife
     {
         // create the grid, of the specified size, that contains Actors
         BoundedGrid<Actor> grid = new BoundedGrid<Actor>(ROWS, COLS);
+        ArrayList<Location> cellList = new ArrayList<Location>();
         
         // create a world based on the grid
         world = new ActorWorld(grid);
@@ -41,6 +44,11 @@ public class GameOfLife
         // display the newly constructed and populated world
         world.show();
         
+    }
+    
+    public void Step()
+    {
+        System.out.println("g");
     }
     
     /**
@@ -55,15 +63,23 @@ public class GameOfLife
         // the grid of Actors that maintains the state of the game
         //  (alive cells contains actors; dead cells do not)
         Grid<Actor> grid = world.getGrid();
-        
-        
     
         // locations of the three cells initially alive
+        /*
         int X1 = 3, Y1 = 1;
         int X2 = 4, Y2 = 2;
         int X3 = 5, Y3 = 3;
         int X4 = 4, Y4 = 4;
         int X5 = 7, Y5 = 5;
+        */
+       
+        int X1 = 3, Y1 = 1;
+        int X2 = 4, Y2 = 2;
+        int X3 = 4, Y3 = 3;
+        int X4 = 3, Y4 = 3;
+        int X5 = 2, Y5 = 3;
+       
+        /*
         int X6 = 8, Y6 = 5;
         int X7 = 9, Y7 = 5;
         int X8 = 8, Y8 = 7;
@@ -78,83 +94,133 @@ public class GameOfLife
         int X17 = 21, Y17 = 6;
         int X18 = 22, Y18 = 7;
         int X19 = 23, Y19 = 7;
+        */
         
         // create and add rocks (a type of Actor) to the three intial locations
         Rock rock1 = new Rock();
         Location loc1 = new Location(Y1, X1);
-        grid.put(loc1, rock1);
+        //grid.put(loc1, rock1);
+        world.add(loc1,rock1);
         
         Rock rock2 = new Rock();
         Location loc2 = new Location(Y2, X2);
-        grid.put(loc2, rock2);
+        world.add(loc2, rock2);
         
         Rock rock3 = new Rock();
         Location loc3 = new Location(Y3, X3);
-        grid.put(loc3, rock3);
+        world.add(loc3, rock3);
         
         Rock rock4 = new Rock();
         Location loc4 = new Location(Y4, X4);
-        grid.put(loc4, rock3);
+        world.add(loc4, rock4);
         
         Rock rock5 = new Rock();
         Location loc5 = new Location(Y5, X5);
-        grid.put(loc5, rock5);
-        
+        world.add(loc5, rock5);
+        /*
         Rock rock6 = new Rock();
         Location loc6 = new Location(Y6, X6);
-        grid.put(loc6, rock6);
+        world.add(loc6, rock6);
         
         Rock rock7 = new Rock();
         Location loc7 = new Location(Y7, X7);
-        grid.put(loc7, rock7);
+        world.add(loc7, rock7);
         
         Rock rock8 = new Rock();
         Location loc8 = new Location(Y8, X8);
-        grid.put(loc8, rock8);
+        world.add(loc8, rock8);
         
         Rock rock9 = new Rock();
         Location loc9 = new Location(Y9, X9);
-        grid.put(loc9, rock9);
+        world.add(loc9, rock9);
         
         Rock rock10 = new Rock();
         Location loc10 = new Location(Y10, X10);
-        grid.put(loc10, rock10);
+        world.add(loc10, rock10);
         
         Rock rock11 = new Rock();
         Location loc11 = new Location(Y11, X11);
-        grid.put(loc11, rock11);
+        world.add(loc11, rock11);
         
         Rock rock12 = new Rock();
         Location loc12 = new Location(Y12, X12);
-        grid.put(loc12, rock12);
+        world.add(loc12, rock12);
         
         Rock rock13 = new Rock();
         Location loc13 = new Location(Y13, X13);
-        grid.put(loc13, rock13);
+        world.add(loc13, rock13);
         
         Rock rock14 = new Rock();
         Location loc14 = new Location(Y14, X14);
-        grid.put(loc14, rock14);
+        world.add(loc14, rock14);
         
         Rock rock15 = new Rock();
         Location loc15 = new Location(Y15, X15);
-        grid.put(loc15, rock15);
+        world.add(loc15, rock15);
         
         Rock rock16 = new Rock();
         Location loc16 = new Location(Y16, X16);
-        grid.put(loc16, rock16);
+        world.add(loc16, rock16);
         
         Rock rock17 = new Rock();
         Location loc17 = new Location(Y17, X17);
-        grid.put(loc17, rock17);
+        world.add(loc17, rock17);
         
         Rock rock18 = new Rock();
         Location loc18 = new Location(Y18, X18);
-        grid.put(loc18, rock18);
+        world.add(loc18, rock18);
         
         Rock rock19 = new Rock();
         Location loc19 = new Location(Y19, X19);
-        grid.put(loc19, rock19);
+        world.add(loc19, rock19);
+        */
+    }
+
+    /**
+     * Checks each cell's boundaries, neighbors, and things like taht.
+     *
+     * @pre        the game has been initialized and the world has been populated with the initial pattern
+     * 
+     */
+    private void checkCells()
+    {
+        Grid<Actor> grid = world.getGrid();
+        
+        ArrayList<Location> occupied = grid.getOccupiedLocations();
+        ArrayList<Location> cellList = new ArrayList<Location>();
+        ArrayList<Location> deaths = new ArrayList<Location>();
+        
+        for (Location loc: occupied)
+        {
+            Actor actor = grid.get(loc);
+            System.out.println("Actor found at: " + loc);
+            System.out.println(grid.getOccupiedAdjacentLocations(loc));
+                        
+            int numNeighbors = grid.getOccupiedAdjacentLocations(loc).size();
+                        
+            System.out.println("Num neighbors: " + numNeighbors);
+            System.out.println("Location: " + loc);
+            if (grid.getEmptyAdjacentLocations(loc) == null)
+            {
+                System.out.println("No neighbor found.");
+                deaths.add(loc);
+            }
+            else if (numNeighbors > 3)
+            {
+                System.out.println("We should remove this one...");
+                deaths.add(loc);
+            }
+            else if (numNeighbors < 2)
+            {
+                System.out.println("We should remove this one as well...");
+                deaths.add(loc);
+            }
+            
+            if (grid.getOccupiedAdjacentLocations(loc).size() == 3) //If there are 3 cells around the current one
+            {
+                cellList.add(loc);
+            }
+        }
     }
 
     /**
@@ -175,29 +241,59 @@ public class GameOfLife
         Grid<Actor> grid = world.getGrid();
         
         Location loc = new Location(0,0);
+
+        System.out.println(grid.getOccupiedLocations());
         
         /* do something like ifneighbor() here for each actor found, iterate until all neighbors are accounted
          * for
         */
-        
+       
+        //We need to start off fresh, so clear the grid first.
+       
         for (int c = 0; c < grid.getNumCols(); c++)
         {
             for (int r = 0; r < grid.getNumRows(); r++)
             {
                 loc = new Location(r,c);
-                Actor actor = world.getGrid().get(loc);
+                Actor actor = grid.get(loc);
                 //System.out.println(actor);
-                if (actor != null)
+                if (actor != null) //If a cell is found...
                 {
-                    //System.out.println("Actor found at: " + loc);
+                    System.out.println("Actor found at: " + loc);
+                    System.out.println(grid.getOccupiedAdjacentLocations(loc));
+                    
+                    int numNeighbors = grid.getOccupiedAdjacentLocations(loc).size();
+                    
+                    System.out.println(numNeighbors);
+                    if (grid.getEmptyAdjacentLocations(loc) == null)
+                    {
+                        System.out.println("No neighbor found.");
+                        actor.removeSelfFromGrid();
+                    }
+                    else if (numNeighbors > 3)
+                    {
+                        System.out.println("We should remove this one...");
+                        actor.removeSelfFromGrid();
+                    }
+                    else if (numNeighbors < 2)
+                    {
+                        System.out.println("We should remove this one as well...");
+                        actor.removeSelfFromGrid();
+                    }
+                }
+                else if (actor == null) //If there is an empty space, look for neighboring cells.
+                {
+                    int numNeighbors = grid.getOccupiedAdjacentLocations(loc).size();
+                    if (numNeighbors == 3)
+                    {
+                        Rock ro = new Rock();
+                        ro.setColor(new Color(255,0,0));
+                        world.add(loc, ro);
+                    }
                 }
                 //System.out.println(grid.getNeighbors(loc));
             }
-            
-            System.out.println(loc);
-            
         }
-        // insert magic here...
         
     }
     
@@ -242,13 +338,13 @@ public class GameOfLife
      *
      */
     public static void main(String[] args)
+    throws InterruptedException
     {
         GameOfLife game = new GameOfLife();
-        //for (int i = 0; i <= game.getNumRows(); i++)
-        //{
-            game.createNextGeneration();
-            game.createNextGeneration();
-        //}
+        for (int i = 0;i<1;i++)
+        {
+            game.checkCells();
+        }
     }
 
 }
